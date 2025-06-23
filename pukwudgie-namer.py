23ba49cbe70bd410fab268f2e91f36a2b75edd98
CogAndStar <jarkosoftroy@gmail.com>
2025-06-22 18:56:35 -0600

    Update pukwudgie-namer
    Added pukwudgie_finals and changed the name generation to randomly decide if first name, last name, both, or neither should have a final consonant

--- pukwudgie-namer.py
+++ pukwudgie-namer.py
@@ -2,12 +2,13 @@ import random
 
 test = True
 
-pukwudgie_initials = ["T", "J", "N", "K", "Qu", "F", "D", "Ch", "M", "G", "V"]
-pukdwugie_medials = ["t", "j", "n", "ff", "ck", "d", "ch", "m", "vv", "gg"]
-pukwudgie_vowels = ["a", "o", "i", "aa", "oo", "ii", "er"]
+pukwudgie_initials = ["T", "J", "N", "K", "Kw", "F", "D", "Ch", "M", "G", "V"]
+pukwudgie_medials = ["t", "j", "n", "ff", "ck", "d", "ch", "m", "v", "g"]
+pukwudgie_vowels = ["a", "o", "i", "a", "o", "i", "ah", "oo", "ee"]
+pukwudgie_finals = ["r", "l", "m", "n"]
 
 print("Welcome to the Pukwudgie Namer!")
-while test = True:
+while test == True:
     number = input("How many names would you like? ")
     try:
         number = float(number)
@@ -16,8 +17,19 @@ while test = True:
         print("Improper input! Try typing an Arabic numeral, pal!")
 
 while number > 0:
-    fname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels)
-    lname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels)
+    x = random.randint(1,6)
+    if x == 1 or x == 2 or x == 3:
+        fname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels)
+        lname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels)
+    if x == 4:
+        fname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_finals)
+        lname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels)
+    if x == 5:
+        fname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels)
+        lname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_finals)
+    if x == 6:
+        fname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_finals)
+        lname = random.choice(pukwudgie_initials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_medials) + random.choice(pukwudgie_vowels) + random.choice(pukwudgie_finals)
     print(fname + " " + lname)
     number -= 1
 
